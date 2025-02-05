268da3ead99513fb5fa0f94c6ba461d93a00c154
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-02-05 18:35:10 +0100

    refactor: convert App class to functios for initialization and rendering

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  new App();
-});
-
-export default class App {
-  constructor() {
-    this.render();
-  }
-
-  render() {
-    const app = document.querySelector('#root');
-    if (!app) return;
-  }
+function renderApp() {
+  const app = document.querySelector('#root');
+  if (!app) return;
 }
+
+function initApp() {
+  renderApp();
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
